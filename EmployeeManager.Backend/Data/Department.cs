b3ea2d6d6ffe301e113242b2429455dd6f394755
faivern <gustaf.faivre@yahoo.com>
2025-11-04 17:25:05 +0100

    feat(structure): added skeleton for backend, mvc client, and db

---
+++ EmployeeManager.Backend/Data/Department.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManager.Backend.Data;
+
+public partial class Department
+{
+    [Key]
+    public int DepartmentId { get; set; }
+
+    [StringLength(100)]
+    public string Name { get; set; } = null!;
+
+    [StringLength(100)]
+    public string? Location { get; set; }
+}
---
+++ EmployeeManager.Backend/Data/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManager.Backend.Data;
+
+public partial class Employee
+{
+    [Key]
+    public int EmployeeId { get; set; }
+
+    [StringLength(50)]
+    public string FirstName { get; set; } = null!;
+
+    [StringLength(50)]
+    public string LastName { get; set; } = null!;
+
+    [StringLength(100)]
+    public string? Email { get; set; }
+
+    [Column(TypeName = "decimal(20, 0)")]
+    public decimal? Salary { get; set; }
+
+    public int DepartmentId { get; set; }
+}
